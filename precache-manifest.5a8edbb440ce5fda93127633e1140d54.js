de3547670d1195dc576d32ee6f45ef0caac476ab
JDVapor <jdvapordestiny@gmail.com>
2019-05-10 11:26:16 -0700

    Updates

---
+++ precache-manifest.5a8edbb440ce5fda93127633e1140d54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "538b4b959c90f59e1349c42bbbf2306f",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "4bb447051da4828c8579",
+    "url": "/clickygame/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickygame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "4bb447051da4828c8579",
+    "url": "/clickygame/static/js/main.d2fbcfb4.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
